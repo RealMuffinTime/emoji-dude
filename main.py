93ab8237d084270cf494fe3d0126ca759541cfbd
RealMuffinTime <realmuffintime@gmail.com>
2021-12-17 14:29:31 +0100

    updated version status

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ from discord.ext import commands
 # New stuff
 #  - Added a limit in `ed.clear <limit>`, you can now specify the amount of messages to delete
 #  - Adds :billed_cap: on every message of user with id 443404465928667137
+#  - Added a voice channel manager for specific channel names
 # Changes
 #  - Typo fixing
 #  - Decreased the maximum emoji amount in 'ed.emojis' to 27
