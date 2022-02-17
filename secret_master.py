7754785856e33228b1a04c0665e0c48519c40e5e
RealMuffinTime <realmuffintime@gmail.com>
2022-02-17 09:08:19 +0100

    change channel name for guild 851244114191319080

--- secret_master.py
+++ secret_master.py
@@ -6,4 +6,4 @@ database_port = 3306
 database_user = ''
 database_pass = ''
 database_name = ''
-managed_channels = [[669895353557975080, "test"], [851244114191319080, "Sprachkanal"], [678556874957783040, "Channel"]]
+managed_channels = [[669895353557975080, "test"], [851244114191319080, "┊Sprachkanal"], [678556874957783040, "Channel"]]
