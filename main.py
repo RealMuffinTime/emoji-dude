4897a9e325c1db5906c74760ac3bd47a9ed0e30c
RealMuffinTime <realmuffintime@gmail.com>
2022-04-07 21:34:03 +0200

    release version 1.1.0

--- main.py
+++ main.py
@@ -6,12 +6,18 @@ from discord.ext import commands
 
 # TODO Index check, members in channel before bot start
 
-# Version 1.0.2 ->
+# Version 1.0.2 -> 1.1.0
 #
 # New stuff
-#  -
+#  - added afk management
+#  - added database support
+#  - added initial help command
+#  - added placeholder for settings command
+#  - added initial backup command
 # Changes
 #  - voice channel manager fix
+#  - rework of cogs
+#  - overall improvements
 
 bot = None
 cogs = None
