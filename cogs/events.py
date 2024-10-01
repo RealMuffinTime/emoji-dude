f7c8bb476e6a5b1d12fd886910cc8029e8fc3a1b
RealMuffinTime <realmuffintime@gmail.com>
2024-10-01 15:44:22 +0200

    move try clauses out of listeners

--- cogs/events.py
+++ cogs/events.py
@@ -31,22 +31,16 @@ class Events(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, ctx):
-        try:
-            await self.auto_poll_thread_creation_command(ctx)
-            if ctx.author.bot:
-                return
-            if ctx.content.startswith("ed."):
-                return
-            await self.auto_reaction_command(ctx)
-        except Exception:
-            utils.on_error("on_message()")
+        await self.auto_poll_thread_creation_command(ctx)
+        if ctx.author.bot:
+            return
+        if ctx.content.startswith("ed."):
+            return
+        await self.auto_reaction_command(ctx)
 
     @commands.Cog.listener()
     async def on_raw_message_edit(self, data):
-        try:
-            await self.auto_poll_thread_creation_command(data)
-        except Exception:
-            utils.on_error("on_raw_message_edit()")
+        await self.auto_poll_thread_creation_command(data)
 
     @commands.Cog.listener()
     async def on_voice_state_update(self, member, before, after):
@@ -56,77 +50,83 @@ class Events(commands.Cog):
 
     @commands.command(name='AutoPollThreadCreation', description='Automatically creates threads on polls, Discord or  `Simple Poll#9879` bot polls are supported.')
     async def auto_poll_thread_creation_command(self, ctx):
-        if isinstance(ctx, discord.RawMessageUpdateEvent):
-            try:
-                guild = self.bot.get_guild(ctx.guild_id)
-                channel = guild.get_channel(ctx.channel_id)
-                message = await channel.fetch_message(ctx.message_id)
+        try:
+            if isinstance(ctx, discord.RawMessageUpdateEvent):
+                try:
+                    guild = self.bot.get_guild(ctx.guild_id)
+                    channel = guild.get_channel(ctx.channel_id)
+                    message = await channel.fetch_message(ctx.message_id)
 
-                webhook_id = int(ctx.data['webhook_id'])
-                if webhook_id != 324631108731928587:
+                    webhook_id = int(ctx.data['webhook_id'])
+                    if webhook_id != 324631108731928587:
+                        return
+
+                    name = message.clean_content.replace("*", "").replace(":bar_chart: ", "")
+                    duration = 1440
+                except AttributeError as e:
+                    return
+            elif isinstance(ctx, discord.message.Message) and type(ctx.channel) == discord.TextChannel:
+                message = ctx
+                if not message.poll:
                     return
 
-                name = message.clean_content.replace("*", "").replace(":bar_chart: ", "")
-                duration = 1440
-            except AttributeError as e:
-                return
-        elif isinstance(ctx, discord.message.Message) and type(ctx.channel) == discord.TextChannel:
-            message = ctx
-            if not message.poll:
+                name = message.poll.question
+                duration = round((message.poll.expires_at - message.poll.created_at).total_seconds()/60)
+            else:
                 return
 
-            name = message.poll.question
-            duration = round((message.poll.expires_at - message.poll.created_at).total_seconds()/60)
-        else:
-            return
-
-        data = await utils.execute_sql(
-            f"SELECT auto_poll_thread_creation_bool_enabled FROM set_guilds WHERE guild_id ='{message.guild.id}'", True)
-        if data[0][0] and message.thread == None:
-            if message.channel.permissions_for(message.guild.me).create_public_threads and message.channel.permissions_for(
-                    message.guild.me).read_message_history:
-                thread = await message.create_thread(
-                    name=name,
-                    auto_archive_duration=duration)
-                await thread.leave()
+            data = await utils.execute_sql(
+                f"SELECT auto_poll_thread_creation_bool_enabled FROM set_guilds WHERE guild_id ='{message.guild.id}'", True)
+            if data[0][0] and message.thread == None:
+                if message.channel.permissions_for(message.guild.me).create_public_threads and message.channel.permissions_for(
+                        message.guild.me).read_message_history:
+                    thread = await message.create_thread(
+                        name=name,
+                        auto_archive_duration=duration)
+                    await thread.leave()
+        except Exception as e:
+            utils.on_error("auto_poll_thread_creation_command()")
 
 
     @commands.command(name='AutoReaction', description='The bot reacts to specific parts in a message with emotes.\n'
                                                        'Supported phrases are `cum`, `poop`,  `cool` and derivations.')
     async def auto_reaction_command(self, ctx):
-        if isinstance(ctx, discord.Message):
-            if ctx.guild:
-                data = await utils.execute_sql(f"SELECT auto_reaction_bool_enabled FROM set_guilds WHERE guild_id ='{str(ctx.guild.id)}'", True)
-                user = ctx.guild.me
-            else:
-                data = [[True]]
-                user = ctx.channel.me
-            if data[0][0]:
-                if ctx.channel.permissions_for(user).add_reactions and ctx.channel.permissions_for(user).read_message_history:
-                    emojis = [
-                        ["cum", "komm", "nut", ["💦"]],
-                        ["shit", "poop", "scheiß", ["💩"]],
-                        ["cool", ["🇨", "🇴", "🅾", "🇱"]],
-                        ["cap", "kappe", ["🧢"]]
-                    ]
+        try:
+            if isinstance(ctx, discord.Message):
+                if ctx.guild:
+                    data = await utils.execute_sql(f"SELECT auto_reaction_bool_enabled FROM set_guilds WHERE guild_id ='{str(ctx.guild.id)}'", True)
+                    user = ctx.guild.me
+                else:
+                    data = [[True]]
+                    user = ctx.channel.me
+                if data[0][0]:
+                    if ctx.channel.permissions_for(user).add_reactions and ctx.channel.permissions_for(user).read_message_history:
+                        emojis = [
+                            ["cum", "komm", "nut", ["💦"]],
+                            ["shit", "poop", "scheiß", ["💩"]],
+                            ["cool", ["🇨", "🇴", "🅾", "🇱"]],
+                            ["cap", "kappe", ["🧢"]]
+                        ]
 
-                    for emoji in emojis:
-                        for text in emoji:
-                            if type(text) is str and text in ctx.content.lower():
-                                for reaction in emoji[-1]:
-                                    try:
-                                        await ctx.add_reaction(reaction)
-                                    except discord.errors.Forbidden:
-                                        pass
+                        for emoji in emojis:
+                            for text in emoji:
+                                if type(text) is str and text in ctx.content.lower():
+                                    for reaction in emoji[-1]:
+                                        try:
+                                            await ctx.add_reaction(reaction)
+                                        except discord.errors.Forbidden:
+                                            pass
+        except Exception as e:
+            utils.on_error("auto_reaction_command()")
 
     @commands.command(name='ManagedAFK', description='Auto moves full muted users after a specific amount of time to the guild set AFK channel.\n'
                                                      'The last used channel is saved, so when a user is no longer full mute, he will be automatically moved to his last voice channel.\n'
                                                      'You need to set an AFK channel and give permissions to move members.')
     async def managed_afk_command(self, member, before, after):
-        if isinstance(member, discord.member.Member):
-            data = await utils.execute_sql(f"SELECT managed_afk_bool_enabled FROM set_guilds WHERE guild_id ='{member.guild.id}'", True)
-            if data[0][0]:
-                try:
+        try:
+            if isinstance(member, discord.member.Member):
+                data = await utils.execute_sql(f"SELECT managed_afk_bool_enabled FROM set_guilds WHERE guild_id ='{member.guild.id}'", True)
+                if data[0][0]:
                     guild = member.guild
                     if not member.bot:
                         member_status = await utils.execute_sql(f"SELECT afk_managed, last_seen, last_channel FROM set_users WHERE user_id ='{str(member.id)}'", True)
@@ -175,15 +175,15 @@ class Events(commands.Cog):
                                 if last_channel is not None:
                                     if guild.afk_channel.permissions_for(guild.me).move_members and last_channel.permissions_for(guild.me).move_members:
                                         await member.move_to(last_channel)
-                except Exception:
-                    utils.on_error("managed_afk_command()")
+        except Exception:
+            utils.on_error("managed_afk_command()")
 
     @commands.command(name='ManagedChannel', description='Automatically creates voice channels when needed, and removes unused.')
     async def managed_channel_command(self, guild):
-        if isinstance(guild, discord.guild.Guild):
-            data = await utils.execute_sql(f"SELECT managed_channel_bool_enabled, managed_channel_voice_channel_channel, managed_channel_ignore_running FROM set_guilds WHERE guild_id ='{guild.id}'", True)
-            if data[0][0]:
-                try:
+        try:
+            if isinstance(guild, discord.guild.Guild):
+                data = await utils.execute_sql(f"SELECT managed_channel_bool_enabled, managed_channel_voice_channel_channel, managed_channel_ignore_running FROM set_guilds WHERE guild_id ='{guild.id}'", True)
+                if data[0][0]:
                     if data[0][2] == 0:
                         await utils.execute_sql(f"UPDATE set_guilds SET managed_channel_ignore_running = 1 WHERE guild_id ='{str(guild.id)}'", False)
 
@@ -234,10 +234,9 @@ class Events(commands.Cog):
                             await utils.execute_sql(f"UPDATE set_guilds SET managed_channel_voice_channel_channel = NULL WHERE guild_id ='{str(guild.id)}'", False)
 
                         await utils.execute_sql(f"UPDATE set_guilds SET managed_channel_ignore_running = 0 WHERE guild_id ='{str(guild.id)}'", False)
-
-                except Exception:
-                    await utils.execute_sql(f"UPDATE set_guilds SET managed_channel_ignore_running = 0 WHERE guild_id ='{str(guild.id)}'", False)
-                    utils.on_error("managed_channel_command()")
+        except Exception:
+            await utils.execute_sql(f"UPDATE set_guilds SET managed_channel_ignore_running = 0 WHERE guild_id ='{str(guild.id)}'", False)
+            utils.on_error("managed_channel_command()")
 
 
 async def setup(bot):
